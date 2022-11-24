3adb57457060494b8d22013f761f208e19cb8ccf
Victor Chukwuka Ogbodo <81800355+chuka00@users.noreply.github.com>
2022-11-24 07:20:27 +0100

    Add files via upload

---
+++ Custom exceptions/Custom exceptions/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace TutlaneExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = null;
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new TutlaneCustomException("Name is Empty");
+            }
+            else
+            {
+                Console.WriteLine("Name: " + name);
+            }
+            Console.ReadLine();
+        }
+    }
+    public class TutlaneCustomException : Exception
+    {
+        public TutlaneCustomException()
+        {
+        }
+        public TutlaneCustomException(string message) : base(message)
+        {
+        }
+        public TutlaneCustomException(string message, Exception innerexception) : base(message, innerexception)
+        {
+        }
+    }
+}
---
+++ Custom exceptions/Custom exceptions/TutlaneCustomException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Custom_exceptions
+{
+    public class TutlaneCustomException : Exception
+    {
+        public TutlaneCustomException()
+        {
+        }
+        public TutlaneCustomException(string message) : base(message)
+        {
+        }
+        public TutlaneCustomException(string message, Exception innerexception) : base(message, innerexception)
+        {
+        }
+    }
+}
